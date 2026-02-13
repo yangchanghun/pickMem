e62abb8aaffe699efc84c646766c7de095fa9df1
yangchanghun <gory4848@naver.com>
2026-02-13 12:36:43 +0900

    타이틀변경

--- client/src/views/vues/mainPage.vue
+++ client/src/views/vues/mainPage.vue
@@ -1,36 +1,44 @@
 <template>
-    <div class="main-page d-flex justify-content-center align-items-center" @click="init();$router.push('/pickMem')">
-        <div class="text-center">
-            <div class="title">
-                <div><strong>Pick The Moment</strong></div>    
-            </div>
-            <div class="sub-title mb-5"><small>무제한으로 원하는 모습을 찍고 꾸미세요!</small></div>
+  <div
+    class="main-page d-flex justify-content-center align-items-center"
+    @click="
+      init();
+      $router.push('/pickMem');
+    "
+  >
+    <div class="text-center">
+      <div class="title">
+        <div><strong>레아포토</strong></div>
+      </div>
+      <div class="sub-title mb-5">
+        <small>무제한으로 원하는 모습을 찍고 꾸미세요!</small>
+      </div>
 
-            <span>Click to Start</span>
-        </div>
-    </div>    
+      <span>Click to Start</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'MainPage',
-    mounted() {
-        this.init();
+  name: "MainPage",
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.$store.commit("resetState");
     },
-    methods: {
-        init() {
-            this.$store.commit('resetState');
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .title {
-    font-size: 60px;
+  font-size: 60px;
 }
 
 .sub-title {
-    font-size: 25px;
+  font-size: 25px;
 }
 </style>
