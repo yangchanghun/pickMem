92a6f402cafa43b131c554454b81453520456601
yangchanghun <gory4848@naver.com>
2026-02-19 11:03:11 +0900

    D

--- client/src/components/footer.vue
+++ client/src/components/footer.vue
@@ -1,49 +1,44 @@
 <template>
-    <div class="footer">
-        <div class="title d-flex justify-content-center h-100">
-            <div class="row m-0 p-0 w-100 m-auto text-center">
-                <div class="col-4">
-                    <a href="https://github.com/dnfwlxo11">제작자 Github 바로가기</a>
-                </div>
-                <div class="col-4">
-                    <strong class="m-auto">{{msg}}</strong>
-                </div> 
-                <div class="col-4">
-                    <a href="https://github.com/dnfwlxo11/pickMem">잘했다고 칭찬하러가기</a>
-                </div>
-            </div>
-            
+  <div class="footer">
+    <div class="title d-flex justify-content-center h-100">
+      <div class="row m-0 p-0 w-100 m-auto text-center">
+        <div class="col-4"></div>
+        <div class="col-4">
+          <strong class="m-auto">{{ msg }}</strong>
         </div>
+        <div class="col-4"></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Footer',
-    props: {
-        msg: {
-            type: String,
-            default: '안녕하세요.'
-        }
-    }
-}
+  name: "Footer",
+  props: {
+    msg: {
+      type: String,
+      default: "rheavsiion",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 a {
-    color: black;
+  color: black;
 }
 
 a:hover {
-    color: grey;
+  color: grey;
 }
 
 .title {
-    font-size: 15px;
+  font-size: 15px;
 }
 
 .footer {
-    height: 50px;
-    border-top: 1px solid lightgrey;
+  height: 50px;
+  border-top: 1px solid lightgrey;
 }
 </style>
